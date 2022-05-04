7df2a5258990fd44265a42fca07384f8c809adfa
Sabrina Maria Andrade Carvalho <sabrinaibm@MacBook-Pro-de-Sabrina.local>
2022-05-04 11:02:00 -0300

    Exercicios em andamento

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+import Primeiro from './components/Primeiro'
+
+export default () => (
+    <View>
+        <Primeiro/>
+    </View>
+)
